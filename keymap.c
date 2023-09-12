5487962f284eb78811a6579c1ece874ac19fe786
OneOfOne <oneofone@gmail.com>
2023-09-11 19:04:05 -0500

    fix timeout

--- keymap.c
+++ keymap.c
@@ -1,3 +1,4 @@
+#include "keycode.h"
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "moonlander.h"
@@ -59,12 +60,12 @@ enum tap_dance_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[QWERTY] = LAYOUT_moonlander(
-		DN(GRV), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,    /* ----------------- */ KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,    /* ----------------- */ KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, MC(EQ),   /* ----------------- */ MC(BR), KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
 		KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_LEAD,  /* ----------------- */ MC(PR), KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,           /* -----------------*/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-		KC_LCTL, LSFTCTL, MO(SYM), KC_LALT, KC_SPC,      KC_MEH, KC_HYPR, KC_SPC,    ALTLBRC, MO(SYM), RSFTCTL, CTLRBRC,
-											KC_ENT, DN(BS), KC_LGUI, KC_APP, DN(DEL), KC_ENT
+		KC_LCTL, LSFTCTL, MO(SYM), KC_LALT, KC_SPC,      KC_MEH, KC_HYPR,          KC_SPC, ALTLBRC, MO(SYM), RSFTCTL, CTLRBRC,
+											KC_ENT, KC_BSPC, KC_LGUI, KC_APP, DN(DEL), KC_ENT
 	),
 
 	[QMAC] = LAYOUT_moonlander(
@@ -78,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[SYM]    = LAYOUT_moonlander(
 		______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, /* ----------------- */ KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR,
-		______, KC_HOME, KC_UP, KC_END, KC_PGUP, ______,  /* ----------------- */ KC_F13, KC_F14, ______, ______, ______, ______, ______, KC_BRK,
+		______, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_INS, KC_F13, /* ----------------- */ KC_F14, ______, ______, ______, ______, ______, KC_BRK,
 		______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, ______, ______, /* ----------------- */  ______, ______, ______, ______, KC_LOCK, ______, ______,
 		______, MS_BTN4, ______, MS_BTN5, ______, ______, /* ----------------- */  ______, ______, ______, ______, ______, ______,
 		______, ______, TO(0), ______, ______, KC_MUTE, /* ----------------- */ KC_MPLY, ______, ______, TO(0), ______, ______,
